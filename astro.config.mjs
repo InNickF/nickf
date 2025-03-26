bf642d2175584f3aa5926221c11b3ed5e9bf7866
innickf <in.nickf@gmail.com>
2025-03-26 13:21:40 +0100

    add some integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nickf.info",
+  base: "/",
+  trailingSlash: "ignore",
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react(), vue()],
+  integrations: [partytown(), react(), vue(), sitemap()],
+  output: "static",
+  adapter: netlify(),
 });
